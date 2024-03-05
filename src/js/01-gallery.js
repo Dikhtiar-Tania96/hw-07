4b2f8606bcba2b0bdf84efbb8fdfcb4280c430a5
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2024-03-05 10:33:37 +0200

    add images map

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+import { galleryItems } from "./gallery-items.js";
+
+// Отримую доступ до елемента, на який буду "вішати" розмітку
+const galleryContainer = document.querySelector('.gallery');
+
+// Cтворюю розмітку
+function createGalleryMarkup(images){
+    return images.map(({preview, original, description}) =>
+    `<li class = 'gallery__item'>
+    <a  class = 'gallery__link' href="${original}">
+      <img class= 'gallery__image'
+      src="${preview}" 
+      alt="${description}">
+    </a>
+  </li>`)
+}
+// Додаю розмітку в DOM
+
+
+// Додаю слухача, який буде слухати клік
+
+
+
+  // Відбираю, щоб ф-ція спрацьовувала лише по кліку на зображення, а за його межами - ні
+
+  // Отримую доступ до дата-атрибуту img, щоб отримати доступ до шляху великого зображення
+
+  // Використовю бібліотеку для відкриття модалки (код повністю з бібліотеки крім шаблоннї розмітки)
+
+
